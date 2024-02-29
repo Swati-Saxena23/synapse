ba1ec4ccb72e640de9d646d2f227b83bbd6fe0f5
Swati-Saxena23 <134062200+Swati-Saxena23@users.noreply.github.com>
2024-02-29 16:34:03 +0530

    Update upload_notebook.py

--- upload_notebook.py
+++ upload_notebook.py
@@ -12,7 +12,7 @@ credential = DefaultAzureCredential(
     client_secret=client_secret,
 )
 
-notebook_path = "path/to/your/notebook.ipynb"
+notebook_path = "addition.ipynb"
 
 with NotebookClient(workspace_url, notebook_path, credential) as notebook_client:
     notebook_client.upload()

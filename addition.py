c0013b90ffa562d1cf4630047f0f0c50680146c0
Swati-Saxena23 <134062200+Swati-Saxena23@users.noreply.github.com>
2024-02-29 16:31:29 +0530

    Update and rename addition.py to upload_notebook.py

--- addition.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to my first Python notebook!")
-a = 5
-b = 10
-sum_result = a + b
-product_result = a * b
-sum_result, product_result
---
+++ upload_notebook.py
@@ -0,0 +1,18 @@
+from azure.identity import DefaultAzureCredential
+from azure.synapse.notebook import NotebookClient
+
+workspace_url = os.environ.get("SYNAPSE_WORKSPACE_URL")
+client_id = os.environ.get("SYNAPSE_CLIENT_ID")
+client_secret = os.environ.get("SYNAPSE_CLIENT_SECRET")
+tenant_id = os.environ.get("SYNAPSE_TENANT_ID")
+
+credential = DefaultAzureCredential(
+    authority="https://login.microsoftonline.com/" + tenant_id,
+    client_id=client_id,
+    client_secret=client_secret,
+)
+
+notebook_path = "path/to/your/notebook.ipynb"
+
+with NotebookClient(workspace_url, notebook_path, credential) as notebook_client:
+    notebook_client.upload()
